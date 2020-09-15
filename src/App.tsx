4d6c9f3116ecb4fedebeb851c395af199a4160ec
outsider <787552171@qq.com>
2020-09-15 11:01:39 +0800

    把global__container改成全局变量

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import './App.scss';
 
+import Home from './view/Home';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="golbal__container">
+        <div className="global__container">
           <div className="App-header-in">
             <div className="header-brand">
               <span>DSV | TRUST CENTER</span>
             </div>
             <div className="header-links">
-              <a href="#">
+              <a href="/">
                 <span>home</span>
               </a>
-              <a href="#"><span>
+              <a href="/"><span>
                 </span>about</a>
             </div>
           </div>
         </div>
       </header>
+      <Home/>
     </div>
   );
 }
---
+++ src/view/Home.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import './Home.style.scss';
+
+const Home:React.FunctionComponent = () => {
+    return (
+        <section className="App-board">
+            <div className="global__container">
+                <div className="App-board-in">
+
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Home;
