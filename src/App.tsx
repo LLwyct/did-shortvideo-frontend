51d0b6adbb14254895ad8310791a8d1f932d7a5f
outsider <787552171@qq.com>
2020-09-15 10:42:53 +0800

    完成顶部header

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <div className="golbal__container">
+          <div className="App-header-in">
+            <div className="header-brand">
+              <span>DSV | TRUST CENTER</span>
+            </div>
+            <div className="header-links">
+              <a href="#">
+                <span>home</span>
+              </a>
+              <a href="#"><span>
+                </span>about</a>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
